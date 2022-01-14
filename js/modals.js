b8ce5c3caaedf67a0a289098892567878334da15
alltheatreides <goldschmidt.antoine@gmail.com>
2022-01-14 16:38:50 +0100

    Bugfix: Exit modal function

--- js/modals.js
+++ js/modals.js
@@ -8,20 +8,23 @@ let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
-  }
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
 }
 
 //-----------------------------Panier-----------------------------
@@ -31,16 +34,16 @@ let modal2 = document.getElementById("myModal2");
 let btn2 = document.getElementById("myBtn2");
 let span2 = document.getElementsByClassName("close2")[0];
 
-btn2.onclick = function() {
+btn2.onclick = function () {
     modal2.style.display = "block";
 }
 
-span2.onclick = function() {
+span2.onclick = function () {
     modal2.style.display = "none";
 }
 
-window.onclick = function(event) {
-    if (event.target == modal2) {
-        modal2.style.display = "none";
-  }
-}
+// window.onclick = function (event) {
+//     if (event.target == modal2) {
+//         modal2.style.display = "none";
+//     }
+// }
