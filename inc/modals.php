ddb55193d9bde3b8ada9c983ac0de652c885eee3
Florian Gabelle <94061619+Flogab59@users.noreply.github.com>
2022-01-14 15:24:39 +0100

    Content:  Modal Mon compte fini
    panier en cours

--- inc/modals.php
+++ inc/modals.php
@@ -1,37 +1,53 @@
-<link rel="stylesheet" href="./css/modals.css" type="text/css" />
 
 
 
 <!-- Trigger/Open The Modal -->
-<!-- <button id="myBtn">Mon compte</button> -->
+<button id="myBtn">Mon compte</button>
+<button id="myBtn2">Panier</button>
 
 <!-- The Modal -->
 <div id="myModal" class="modal">
 
   <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close" onclick="closeModal('myModal')">&times;</span>
+    <div class="modal-content">
+    <span class="close">&times;</span>
     <h2>Mon compte</h2>
     <form>
         <legend>J'ai déjà un compte</legend>
-        
-        <label>Mon adresse email</label>
-        <input class="input" type="mail" name="mail"/>
-        
-        <label>Mon mot de passe</label>
-        <input class="input" type="password" name="password"/>
-        
-        <a href="#" class="connect">Se connecter</a>
-        <a href="#" id="mdpOublie">J'ai oublié mon mot de passe</a>
+        <div class="field">
+            <label>Mon adresse email</label>
+            <input class="input" type="mail" name="mail"/>
+        </div>
+        <div class="field">
+            <label>Mon mot de passe</label>
+            <input class="input" type="password" name="password"/>
+        </div>
+        <div class="field">
+            <a href="#" class="connect">Se connecter</a>
+            <a href="#" id="mdpOublie">J'ai oublié mon mot de passe</a>
+        </div>
     </form>
     
     <form>
-        <legend>Je n'ai pas de compte</legend>
+        <legend id="noAcc">Je n'ai pas de compte</legend>
         <a href="#" class="connect">Créer un compte</a>
     </form>
     
-  </div>
-
+    </div>
 </div>
 
-<script type="text/javascript" src="./js/modals.js"></script>
+
+<div id="myModal2" class="modal2">
+
+  <!-- Modal content -->
+    <div class="modal-content">
+    <span class="close2">&times;</span>
+    <h2>Mon panier</h2>
+    <div>Aucun article</div>
+    
+    </div>
+</div>
+
+
+
+
--- js/modals.js
+++ js/modals.js
@@ -1,23 +1,46 @@
-// Function to open a modal in an onclick action
-// @modalID is an argument that takes the relevant modal ID
-function openModal(modalID) {
-    let modal = document.getElementById(modalID)
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Function to close a modal in an onclick action
-// @modalID is an argument that takes the relevant modal ID
-function closeModal(modalID) {
-    let modal = document.getElementById(modalID)
-
-    document.getElementsByClassName("close")[0].onclick = () => {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function (e) {
-        if (e.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+  }
+}
+
+//-----------------------------Panier-----------------------------
+//----------------------------------------------------------------
+
+let modal2 = document.getElementById("myModal2");
+let btn2 = document.getElementById("myBtn2");
+let span2 = document.getElementsByClassName("close2")[0];
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+  }
+}
