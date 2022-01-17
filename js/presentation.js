7171a448d6dd41c8cd481e6242c5677c95c2b217
julienh62 <78169249+julienh62@users.noreply.github.com>
2022-01-17 13:51:16 +0100

    img slider

--- js/presentation.js
+++ js/presentation.js
@@ -2,27 +2,64 @@
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.minislider1').addEventListener('click', zoomSlider);
+    document.querySelector('.minislider1').addEventListener('click', zoomSlider1);
     console.log("connexion js ok")
 });
 
-function zoomSlider() {
+function zoomSlider1() {
     let a = document.querySelector(".minislider1");
+   
+    a.classList.remove("minislider1");
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.minislider2').addEventListener('click', zoomSlider2);
+});
+
+function zoomSlider2() {
     //  let b = document.querySelector(".minislider2").style.display = none;
     //document.getElementById("noneminislider2").style.display = none;
     let b = document.querySelector(".minislider2");
-    let c = document.querySelector(".minislider3");
-    let d = document.querySelector(".minislider4");
-    let e = document.querySelector(".minislider5");
-
-    //  console.log(a);
-    a.classList.remove("minislider1");
     b.classList.remove("minislider2");
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.minislider3').addEventListener('click', zoomSlider3);
+    console.log("connexion js ok")
+});
+
+function zoomSlider3() {
+    let c = document.querySelector(".minislider3");
     c.classList.remove("minislider3");
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.minislider4').addEventListener('click', zoomSlider4);
+    console.log("connexion js ok")
+});
+
+function zoomSlider4() {
+    let d = document.querySelector(".minislider4");
     d.classList.remove("minislider4");
+
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.minislider5').addEventListener('click', zoomSlider5);
+    console.log("connexion js ok")
+});
+
+function zoomSlider5() {
+    let e = document.querySelector(".minislider5");
     e.classList.remove("minislider5");
 
-
-    ;
-    
 }
+
+
+
+
+
+
+
+
+
+
