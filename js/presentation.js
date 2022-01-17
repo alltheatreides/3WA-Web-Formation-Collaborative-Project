33ef224af71482a5ecaea2258208f99933ced6a2
julienh62 <78169249+julienh62@users.noreply.github.com>
2022-01-17 12:34:22 +0100

    update

--- js/presentation.js
+++ js/presentation.js
@@ -1,13 +1,11 @@
 'use strict';    
 
 
-        document.addEventListener('DOMContentLoaded', function(){
-            document.querySelector('.minislider1').addEventListener('click', zoomSlider);
-           console.log("connexion js ok")
-       });
-        function zoomSlider(){
-  let a = document.querySelector(".minislider1");
-
-           a.classList.remove(".minislider1"); 
-        }
-    
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('.minislider1').addEventListener('click', zoomSlider);
+    console.log("connexion js ok")
+});
+function zoomSlider(){
+    let a = document.querySelector(".minislider1");
+    a.classList.remove(".minislider1"); 
+}
