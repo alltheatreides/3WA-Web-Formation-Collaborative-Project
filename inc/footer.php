9a695803296b9164d785a548af061a6a5f8feda4
julienh62 <78169249+julienh62@users.noreply.github.com>
2022-01-17 09:42:04 +0100

    Update footer.php
    update footer

---
+++ inc/footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+
+    <!--fontawesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+    <link rel="stylesheet" href="../css/footer.css">
+
+</head>
+<footer>
+    <nav class="navFooter">
+       <a href="#" class="fas fa-desktop">Ordinateurs</a>
+        <a href="#" class="fas fa-database">Périphériques & composants</a>
+        <a href="#" class="fas fa-mobile-alt">Téléphones</a>
+        <a href="#" class="far fa-clock">Objets connectés</a>
+
+
+    </nav>
+
+    <div class="informations">
+
+        <section class="apropos">
+            <h3>Moyen de paiement</h3>
+            <div class="carte">
+                <img src="../img/paiements.JPG" alt="carte bancaire">
+            </div>
+        </section>
+
+        <section class="apropos">
+            <h2>Sur les réseaux sociaux</h2>
+            <div class="ico">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-youtube"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </section>
+
+        <section class="apropos">
+                        <h2>A PROPOS</h2>
+            <a href="#" class="nous">Qui sommes nous?</a>
+            <a href="#" class="nous">Nos services</a>
+            <a href="#" class="nous">Contactez-nous</a>
+        </section>
+       
+        <section class="apropos">
+                     <h2>A PROPOS</h2>
+            <a href="#" class="nous">Plan du site</a>
+            <a href="#" class="nous">Foire aux questions</a>
+            <a href="#" class="nous">Conditions générales de vente</a>
+            <a href="#" class="nous">information légales</a>
+        </section>
+
+
+    </div>
+   <div class="license">
+        <a rel="license" href="https://3wa.fr/propriete-materiel-pedagogique/"><img alt="Propriété de la 3W Academy" style="border-width:0" src="https://3wa.fr/wp-content/themes/3wa2015/img/logos/big.png"></a>
+        <p>Cet exercice de 3W Academy est mis à disposition pour l'usage personnel des étudiants.Pas de Rediffusion - Attribution - Pas d'utilisation Commerciale - Pas de modification - International. Les autorisations au-delà du champs de cette license peuvent être obtenues auprès de contact contact@3wa.fr. Maquette réalisée par stephane Pereck.</p>
+    </div>
+
+
+
+
+
+</footer>
+
+
+</html>
