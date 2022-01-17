42e85ef1e88cfcabf3048a60840e0541eca9c338
julienh62 <78169249+julienh62@users.noreply.github.com>
2022-01-17 13:40:02 +0100

    img slider

--- js/presentation.js
+++ js/presentation.js
@@ -1,11 +1,28 @@
-'use strict';    
+'use strict';
 
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.minislider1').addEventListener('click', zoomSlider);
     console.log("connexion js ok")
 });
-function zoomSlider(){
+
+function zoomSlider() {
     let a = document.querySelector(".minislider1");
-    a.classList.remove(".minislider1"); 
+    //  let b = document.querySelector(".minislider2").style.display = none;
+    //document.getElementById("noneminislider2").style.display = none;
+    let b = document.querySelector(".minislider2");
+    let c = document.querySelector(".minislider3");
+    let d = document.querySelector(".minislider4");
+    let e = document.querySelector(".minislider5");
+
+    //  console.log(a);
+    a.classList.remove("minislider1");
+    b.classList.remove("minislider2");
+    c.classList.remove("minislider3");
+    d.classList.remove("minislider4");
+    e.classList.remove("minislider5");
+
+
+    ;
+    
 }
